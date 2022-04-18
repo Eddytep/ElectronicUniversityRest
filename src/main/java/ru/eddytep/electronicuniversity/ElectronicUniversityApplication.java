ea420028590eae915afd93f5db1cd26761283b17
Eduard Teplitsky <eddytep@mail.ru>
2022-04-18 15:04:01 +0300

    Initialize Vue.js project "front"

---
+++ src/main/java/ru/eddytep/electronicuniversity/ElectronicUniversityApplication.java
@@ -0,0 +1,13 @@
+package ru.eddytep.electronicuniversity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElectronicUniversityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ElectronicUniversityApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/eddytep/electronicuniversity/ElectronicUniversityApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.eddytep.electronicuniversity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElectronicUniversityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
